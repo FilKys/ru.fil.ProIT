343a59e4508b47d723f8a34c405ec55eee1bd554
milli <million.fil@mail.ru>
2020-04-10 17:25:34 +0500

    Сделана загрузка файлов на сервер

--- src/main/java/API/Application.java
+++ src/main/java/API/Application.java
@@ -1,5 +1,8 @@
+package API;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class Application {
---
+++ src/main/java/API/Controllers/ControllerRequest.java
@@ -0,0 +1,14 @@
+package API.Controllers;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+@RestController
+public class ControllerRequest {
+
+
+}
